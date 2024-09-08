a748cf61ba767eb916381c53c21b0720c031e4ca
Md Shamim Reza <mdshamimreza@Mds-MacBook-Pro.local>
2024-09-08 22:33:53 +0600

    written for medium

--- Searching/linearSearch.js
+++ Searching/linearSearch.js
@@ -1,18 +1,18 @@
-let arr = [1, 2, 3, 4, 5];
+let arr = [3, 8, 11, 34, 1];
 
-const target = 6;
+// you should find the index of the target number in the array using a linear search algorithm
+const target = 51;
 
-function linearSearch(arr, target) { 
-    for (let i = 0; i < arr.length; i++){
-        console.log(i)
-        if (arr[i] === target) { 
-           return `Element ${target} found at index ${i}`;
-             
-        }
+function linearSearch(arr, target) {
+  // iterate over each element in the array and check if it matches the target
+  for (let i = 0; i < arr.length; i++) {
+    // if the current element matches the target, return its index
+    if (arr[i] === target) {
+      return i;
     }
-            return "not found in the array";
-
+  }
+  // if no match is found, return -1
+  return -1;
 }
 
-console.log(linearSearch(arr, target))
-
+console.log(linearSearch(arr, target));
