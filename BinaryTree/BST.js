21d44d9fb6fcfe7c9820712ed9a9e3a6cf46bfa8
Md Shamim Reza <mohammadshamimreza23393@gmail.com>
2024-10-24 07:38:51 +0600

    insert in BST

--- BinaryTree/BST.js
+++ BinaryTree/BST.js
@@ -12,8 +12,33 @@ class BinarySearchTree {
         this.root = new Node(value)
     }
 
+    insert(value) { 
+        let currentNode = this.root;
+        while (true) {
+            if (currentNode.value < value) {
+                if (currentNode.right === null) { 
+                    let node = new Node(value)
+                    currentNode.right = node;
+                    break;
+
+                }
+                currentNode = currentNode.right;
+            } else {
+                  if (currentNode.left === null) { 
+                    let node = new Node(value)
+                    currentNode.left = node;
+                    break;
+
+                  }
+                currentNode = currentNode.left;
+            }
+        }
+    }
+
 }
 
 let bst = new BinarySearchTree(10);
+bst.insert(25);
+bst.insert(15);
 
-bst
+console.log(bst)
