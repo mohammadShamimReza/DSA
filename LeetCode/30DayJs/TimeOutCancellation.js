17171b53a26dddee5bbc859f8c6542a5640efcbb
mohammadshamimreza <mohammadshamimreza23393@gmail.com>
2025-01-16 21:36:54 +0600

    solve 2715

--- LeetCode/30DayJs/TimeOutCancellation.js
+++ LeetCode/30DayJs/TimeOutCancellation.js
@@ -1,15 +1,14 @@
 var cancellable = function (fn, args, t) {
+  const cancelFn = () => {
+    clearTimeout(timer);
+  };
+  const timer = setTimeout(() => {
+    fn(...args);
+  }, t);
 
-    const time = () => {
-        setTimeout(fn(args), t);
-
-    }
-
-    return cancelFn = () => {
-        clearTimeout(time)
-    };
+  return cancelFn;
 };
 
 const res1 = cancellable((fn = (x) => x * 5), (args = [2]), (t = 20));
 
-console.log(res1)
+console.log(res1);
